a6332c0199f4cdeef2e090accc5e13f7de14b277
CestnyTR <78599043+CestnyTR@users.noreply.github.com>
2023-03-28 23:43:00 +0300

    sc

--- src/index.js
+++ src/index.js
@@ -1,7 +1,25 @@
-// const menuBtn=document.getElementById("hamburger")
-// const navList=document.getElementById("menu-list")
-// function menuBtnClick(){
-// navList.classList.toggle("show")}menuBtn.addEventListener("click",menuBtnClick)
+window.addEventListener("wheel", function(event) {
+  if (event.deltaY > 0) {
+    var windowHeight = window.innerHeight;
+    var fullHeight = document.documentElement.scrollHeight;
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    var remainingHeight = fullHeight - (scrollTop + windowHeight);
+    if (remainingHeight > windowHeight) {
+      window.scrollBy(0, windowHeight);
+    } else if (remainingHeight > 0) {
+      window.scrollBy(0, remainingHeight);
+    }
+  }
+  if (event.deltaY < 0) {
+    var windowHeight = window.innerHeight;
+    var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (scrollTop > windowHeight) {
+      window.scrollBy(0, -windowHeight);
+    } else if (scrollTop > 0) {
+      window.scrollBy(0, -scrollTop);
+    }
+  }
+});
 
 
 //! Send Mail
@@ -12,14 +30,15 @@ function sendMail() {
     email: document.querySelector('#email').value,
     message: document.querySelector('#message').value,
   };
-
-  const serviceID = "service_07nekqk";
-  const templateID = "template_e8e9sm7";
-  emailjs.send(serviceID, templateID, params).then(
-    res => {
-      document.querySelector('#firstName') = "";
-      document.querySelector('#surname') = "";
-      document.querySelector('#email') = "";
-      document.querySelector('#message') = "";
-    }).catch(err => console.log(err));
+  if (params.name.length >= 5 && params.surname.length >= 5 && params.email.length >= 5 && params.message.length >= 15) {
+    const serviceID = "service_07nekqk";
+    const templateID = "template_e8e9sm7";
+    emailjs.send(serviceID, templateID, params).then(
+      res => {
+        document.querySelector('#firstName').value = "";
+        document.querySelector('#surname').value = "";
+        document.querySelector('#email').value = "";
+        document.querySelector('#message').value = "";
+      }).catch(err => console.log("Error" + err));
+  } else alert("lütfen formu doldurunuz.");
 }
